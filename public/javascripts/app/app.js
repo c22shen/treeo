f7bd5963ff144876d74a152ad89093ce68ff8cef
Xiao Shen <xiaoshen2009@gmail.com>
2015-08-01 02:36:50 -0400

    install bootstrap fontawesome bootstrap-ui

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular
-	.module('app', ['ui.router'])
-	.config([function(){
+	.module('app', ['ui.router', 'ui.bootstrap'])
+	.controller('ButtonsCtrl', function ($scope) {
+  $scope.singleModel = 1;
 
-	}]);
+  $scope.radioModel = 'Middle';
+
+  $scope.checkModel = {
+    left: false,
+    middle: true,
+    right: false
+  };
+});
+	
